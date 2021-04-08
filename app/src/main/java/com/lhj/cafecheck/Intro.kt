59ae73d6ae47802b04a4e802b86a2415d655f2f3
user <eudj465@gmail.com>
2021-04-08 20:21:37 +0900

    1. Create CafeCheck Project

--- app/src/main/java/com/lhj/cafecheck/Intro.kt
+++ app/src/main/java/com/lhj/cafecheck/Intro.kt
@@ -3,10 +3,11 @@ package com.lhj.cafecheck
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
+
 
     }
 }
